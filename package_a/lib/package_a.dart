e7d85569fb83626df829bc25a87a10ae0f30ae6b
Brad Kwon <hyeon-gu.kwon@healy.world>
2025-12-11 12:05:58 +0100

    revert!: renames calculator class back
    BREAKING CHANGE: calculator class name is reverted back to the original name

--- package_a/lib/package_a.dart
+++ package_a/lib/package_a.dart
@@ -1,5 +1,5 @@
 /// A Calculator.
-class CalculatorA {
+class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
   int minusThree(int value) => value - 3;
--- package_a/test/package_a_test.dart
+++ package_a/test/package_a_test.dart
@@ -4,7 +4,7 @@ import 'package:package_a/package_a.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = CalculatorA();
+    final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
