f97cb1b081312953f82beabec5fa879a0c13f2a0
Brad Kwon <hyeon-gu.kwon@healy.world>
2025-12-11 12:03:33 +0100

    chore!: drop minusOne and minusTwo

--- package_a/lib/package_a.dart
+++ package_a/lib/package_a.dart
@@ -2,8 +2,5 @@
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
-
-  int minusOne(int value) => value - 1;
-  int minusTwo(int value) => value - 2;
   int minusThree(int value) => value - 3;
 }
